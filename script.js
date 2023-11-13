466b9c08bef2ed61b2d5446ab2a531a3b1cbfd0f
Jameel Ahmed <126319130+jameel-webdev@users.noreply.github.com>
2023-11-13 12:32:00 +0530

    changed var into const

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 //Writing Function for Creating Div Element
 function div(tagname, atname, atvalue, content) {
-    var ele = document.createElement(tagname);
+    const ele = document.createElement(tagname);
     ele.setAttribute(atname, atvalue);
     ele.innerHTML = content;
     return ele;
 }
 // Writing Functions For Creating Button Elements
 function buttn(tagname, atname, atvalue, atnam1, atvalue1, content) {
-    var b = document.createElement(tagname);
+    const b = document.createElement(tagname);
     b.setAttribute(atname, atvalue);
     b.setAttribute(atnam1, atvalue1);
     b.innerHTML = content;
     return b;
 }
 // Creating Calculator Display
-var cal = div("div", "class", "calculator", " ");
-var caldisplay = div("div", "id", "cal-display", "");
-var h1 = div("h1", "id", "displayNo", "");
+const cal = div("div", "class", "calculator", " ");
+const caldisplay = div("div", "id", "cal-display", "");
+const h1 = div("h1", "id", "displayNo", "");
 
 //Creating buttons
-var allbuttons = div("div", "class", "all-btns", "");
-var b7 = buttn("button", "class", "nos", "id", "7", "7");
-var b8 = buttn("button", "class", "nos", "id", "8", "8");
-var b9 = buttn("button", "class", "nos", "id", "9", "9");
-var division = buttn("button", "class", "nos", "id", "/", "/");
-var b4 = buttn("button", "class", "nos", "id", "4", "4");
-var b5 = buttn("button", "class", "nos", "id", "5", "5");
-var b6 = buttn("button", "class", "nos", "id", "6", "6");
-var multiplication = buttn("button", "class", "nos", "id", "*", "*");
-var b1 = buttn("button", "class", "nos", "id", "1", "1");
-var b2 = buttn("button", "class", "nos", "id", "2", "2");
-var b3 = buttn("button", "class", "nos", "id", "3", "3");
-var subtraction = buttn("button", "class", "nos", "id", "-", "-");
-var bclear = buttn("button", "class", "btn-clear", "id", "clear", "C");
-var b0 = buttn("button", "class", "nos", "id", "0", "0");
-var equalto = buttn("button", "class", "btn-equal", "id", "equal-sign", "=");
-var addition = buttn("button", "class", "nos", "id", "+", "+");
+const allbuttons = div("div", "class", "all-btns", "");
+const b7 = buttn("button", "class", "nos", "id", "7", "7");
+const b8 = buttn("button", "class", "nos", "id", "8", "8");
+const b9 = buttn("button", "class", "nos", "id", "9", "9");
+const division = buttn("button", "class", "nos", "id", "/", "/");
+const b4 = buttn("button", "class", "nos", "id", "4", "4");
+const b5 = buttn("button", "class", "nos", "id", "5", "5");
+const b6 = buttn("button", "class", "nos", "id", "6", "6");
+const multiplication = buttn("button", "class", "nos", "id", "*", "*");
+const b1 = buttn("button", "class", "nos", "id", "1", "1");
+const b2 = buttn("button", "class", "nos", "id", "2", "2");
+const b3 = buttn("button", "class", "nos", "id", "3", "3");
+const subtraction = buttn("button", "class", "nos", "id", "-", "-");
+const bclear = buttn("button", "class", "btn-clear", "id", "clear", "C");
+const b0 = buttn("button", "class", "nos", "id", "0", "0");
+const equalto = buttn("button", "class", "btn-equal", "id", "equal-sign", "=");
+const addition = buttn("button", "class", "nos", "id", "+", "+");
 
 
 allbuttons.append(b7, b8, b9, division, b4, b5, b6, multiplication, b1, b2, b3, subtraction, bclear, b0, equalto, addition);
